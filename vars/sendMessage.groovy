194335244331fc4c98a5ceb493cb1e9ea4a68d67
ivanovEV <ivanovev@melonfashion.com>
2022-04-12 14:33:57 +0300

    .

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -5,7 +5,7 @@ def call(String message) {
             throw new Exception('Message is empty!')
         }
         String authToken = '1145915027:AAHEtQJGSTERyttKvXXQxd4Li1ax6aMNj-U'
-        String urlString = "https://api.telegram.org/bot${authToken}}/sendMessage"
+        String urlString = "https://api.telegram.org/bot${authToken}/sendMessage"
 
         httpRequest httpMode: 'POST', requestBody: '''{"chat_id": 436258240,
         "parse_mode": "MarkdownV2",
