0f606570353c08544ee62f86a692fa612d084812
ivanovEV <ivanovev@melonfashion.com>
2022-04-12 23:30:35 +0300

    .

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -4,16 +4,8 @@ import ru.telegramNotify.ioc.ContextRegistry
 import ru.telegramNotify.botManager
 import ru.telegramNotify.message
 
-def call(String text, int chat_id = "", String parse_mode = "") {
+def call(String text, int chat_id = env.TELEGRAM_CHAT.toInteger(), String parse_mode = "MarkdownV2") {
        ContextRegistry.registerDefaultContext(this)
-
-      if (chat_id.trim().length() == 0){
-            chat_id = env.TELEGRAM_CHAT
-      }
-      if (parse_mode.trim().length() == 0){
-            parse_mode = "MarkdownV2"
-      }
-      
       def message = new message(
             chat_id: chat_id,
             text: text,
