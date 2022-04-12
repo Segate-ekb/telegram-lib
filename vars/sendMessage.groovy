2dfa2fe480d8432703ea47ed8c62e0a863b096e2
ivanovEV <ivanovev@melonfashion.com>
2022-04-12 18:06:34 +0300

    .

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 import groovy.json.*
 import ru.telegramNotify.ioc.ContextRegistry
+import ru.telegramNotify.botManager
 
-def call(message, authToken = null) {
+def call(message) {
          ContextRegistry.registerDefaultContext(this)
          def telegram = new botManager()
           telegram.setAuthToken('1145915027:AAHEtQJGSTERyttKvXXQxd4Li1ax6aMNj-U')
