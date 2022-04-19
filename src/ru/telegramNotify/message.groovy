a32fe7616e24d7d8c8912cef30304d1f90185cf2
ivanovEV <ivanovev@melonfashion.com>
2022-04-19 16:43:42 +0300

    .

--- src/ru/telegramNotify/message.groovy
+++ src/ru/telegramNotify/message.groovy
@@ -6,7 +6,7 @@ import groovy.json.*
 class message {
     String text
     long chat_id
-    String parse_mode = "MarkdownV2"
+    String parse_mode = "Markdown"
     Boolean disable_web_page_preview
     Boolean disable_notification
     int reply_to_message_id
