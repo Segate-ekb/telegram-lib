75e238bc5c57d200875364969f67af884b8e4407
ivanovEV <ivanovev@melonfashion.com>
2022-04-19 15:01:46 +0300

    .

--- src/ru/telegramNotify/message.groovy
+++ src/ru/telegramNotify/message.groovy
@@ -13,6 +13,6 @@ class message {
     int reply_to_message_id
 
     def serializeMessage(){
-     return JsonOutput.prettyPrint(JsonOutput.toJson(this, "UTF-8"))
+     return StandardCharsets.UTF_8.encode(JsonOutput.prettyPrint(JsonOutput.toJson(this)))
     }
 }
