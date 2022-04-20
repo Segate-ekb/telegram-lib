daf9a4c15fa3c7db53f5d1051460b760b3fd1cb0
ivanovEV <ivanovev@melonfashion.com>
2022-04-20 16:30:28 +0300

    ю

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -4,8 +4,9 @@ import ru.telegramNotify.ioc.ContextRegistry
 import ru.telegramNotify.botManager
 import ru.telegramNotify.message
 
-def call(String text, long chat_id = Long.valueOf(env.TELEGRAM_CHAT), String parse_mode = "Markdown") {
+def call(String text, def chat_id = Long.valueOf(env.TELEGRAM_CHAT), String parse_mode = "Markdown") {
        ContextRegistry.registerDefaultContext(this)
+       chat_id =  Long.valueOf(chat_id); //Если передадут строку
       def message = new message(
             chat_id: chat_id,
             text: text,
